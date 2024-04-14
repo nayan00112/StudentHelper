12e83cdb0e110ed2d54b98b7cdc7e5732b0df15a
Nayan Patel <nayanpatel00112@gmail.com>
2024-04-14 12:49:00 +0530

    solve navbar responciveness

--- StudentHelper/static/home/nav.js
+++ StudentHelper/static/home/nav.js
@@ -1,14 +1,14 @@
 
 let s = 0;
 function navclick() {
-if (s == 0){
-    document.getElementsByClassName('fordesk')[0].style = "display:flex;"
-    document.getElementsByClassName('fordesk')[1].style = "display:flex;"
-    s = 1;
-}
-else{
-    document.getElementsByClassName('fordesk')[0].style = "display:none;"
-    document.getElementsByClassName('fordesk')[1].style = "display:none;"
-    s = 0;
-}
+    if (s == 0) {
+        document.getElementsByClassName('fordesk')[0].style = "display:flex;";
+        document.getElementsByClassName('fordesk')[1].style = "display:flex;";
+        s = 1;
+    }
+    else {
+        document.getElementsByClassName('fordesk')[0].style = "display:none;";
+        document.getElementsByClassName('fordesk')[1].style = "display:none;";
+        s = 0;
+    }
 }
